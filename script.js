cb2f99983c632ef4639f3c39754697842fbe34c0
DESKTOP-5NG7671\Timo Bellartz <bellartz.timo@gmail.com>
2022-03-14 19:22:16 +0100

    Fixed left to right and right to left translation

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ import 'codemirror/addon/edit/closebrackets.js'
 var graphId;
 var initialGraph;
 var roomNr;
-
-var lastGraph;
+var tutorial;
 
 var autoSave = true;
 var timeoutHandle;
@@ -24,7 +23,6 @@ var timeoutHandle;
 var forceGraph;
 var initialGraph;
 var lastNode;
-var edgeFormat;
 // Is the current client the one that "owns" the graph
 var host = false;
 // Current states of the buttons
@@ -36,8 +34,7 @@ var nodeSelected = false;
 ////////// Handle Buttons /////////////
 
 $("#insertGraph").on('click', function(){
-    let force = true;
-    loadXml(force);
+    loadXml();
 })
 $("#apply").on('click', function(){
     applyChanges();
@@ -80,29 +77,44 @@ const editor = CodeMirror(editorContainer, {
     mode: 'xml',
     autoCloseBrackets : true,
     autoCloseTags : true,
-    lineNumbers: true,
+    lineNumbers: true
   })
+  provider.disconnect();
 
+// detect change and update visualization after 500 ms
 editor.on('change', function(cm){
     if(autoSave){
         window.clearTimeout(timeoutHandle);
         timeoutHandle = window.setTimeout(function(){
             applyChanges(false);
-        }, 0)
+        }, 500)
     }
 })
-
 window.addEventListener('load', () => {
     graphId = getUrlVar("graphId");
+
+    // Initialize editor and load graph or set standard graph if the user is not the host
     if(typeof graphId === 'undefined'){
-        initialGraph = {nodes: [{color: 'rgba(204.0,204.0,255.0,1.0)', size: 20, name: 0, id: 0, label: ''}], links: []};
+        initialGraph = {nodes: [
+            {color: 'rgba(204.0,204.0,255.0,1.0)', size: 20, name: "0", id: "0", label: ''},
+            {color: 'rgba(204.0,204.0,255.0,1.0)', size: 20, name: "1", id: "1", label: ''},
+            {color: 'rgba(204.0,204.0,255.0,1.0)', size: 20, name: "2", id: "2", label: ''}],
+            links: [{style: 0,
+                 source: "0",
+                 target: "1"},
+                 {style: 0,
+                    source: "1",
+                    target: "2"},
+                {style: 0,
+                    source: "2",
+                    target: "0"}
+                ]};
         getForceVis();
     } else {
         host = true;
         initialize();
     }
     
-    // getXmlGraph();
     const copyBtn = $('#copyUrl-btn')[0];
     let url =  window.location.origin + window.location.pathname + "?room=" + roomNr;
     $('#copy-url').val(url);
@@ -111,21 +123,26 @@ window.addEventListener('load', () => {
         copyText.select();
         navigator.clipboard.writeText(url);
     })
+    
+    // Set editor size with standard text
     editorContainer.setAttribute('id', 'editor');
     document.body.insertBefore(editorContainer, null)
-
     editor.setSize("60%","700");
-
+    ytext.insert(0, tutorial);
     // Bind the codemirror editor to yjs
     const binding = new CodemirrorBinding(ytext, editor, provider.awareness)
-    
-    // Close connection or connect again
+
+    // Connection button
     const connectBtn = (document.getElementById('y-connect-btn'))
     connectBtn.addEventListener('click', () => {
       if (provider.shouldConnect) {
         provider.disconnect()
         connectBtn.textContent = 'Connect'
       } else {
+          if(!host){
+            editor.setValue("");
+            editor.clearHistory();
+          }
         provider.connect()
         connectBtn.textContent = 'Disconnect'
       }
@@ -150,10 +167,10 @@ function uuidv4() {
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
-function loadXml(force){
+function loadXml(){
     editor.setValue("");
     editor.clearHistory();
-    let xml = jsonToXmlL(forceGraph.graphData(), force)
+    let xml = jsonToXmlL(forceGraph.graphData())
     ytext.insert(0, xml);
 }
 // Get the Graph visualization and set up visualozation
@@ -161,6 +178,7 @@ function initialize() {
     sendRequest("get", "visualization/graph/" + graphId + "/outputFormat/JSON/layout/ORGANIC", "",
         function (response) {
             initialGraph = JSON.parse(response);
+            console.log(initialGraph);
             getForceVis();
         },
         /* Error handler */
@@ -171,54 +189,67 @@ function initialize() {
             showConnectionErrorMessage("Visualization was not received.", errorData);
         });
 }
+// Translate the xml to a JSON format and look for changes in the graph and apply those
+
 function applyChanges(edgeUpdate=true){
-    // const { nodes, links } = forceGraph.graphData();
+    const { nodes, links } = forceGraph.graphData();
 
-    // let updated = xmlToJson(ytext.toString());
-    // let uNodes = updated.nodes;
-    // let uLinks = updated.links;
-    // for(var key in nodes){
-    //     check: {
-    //         for(var key2 in uNodes){
-    //             console.log("n: " + nodes[key].id + ", uN: " + uNodes[key2].id);
-    //             if(nodes[key].id == uNodes[key2].id){
-    //                 uNodes.splice(key2, 1);
-    //                 break check;
-    //             } 
-    //             if(key2 == uNodes.length - 1){
-    //                 console.log("hi");
-    //                 nodes.splice(key, 1);
-    //             }
-    //         }
-    //     }
+    let updated = xmlToJson(ytext.toString());
+    let uNodes = updated.nodes;
+    let uLinks = updated.links;
+    let delNodes = [];
+    console.log(uLinks);
+    console.log(links);
+    // Remove deleted nodes from the original array
+    for(var key in nodes){
+        if(!(uNodes.some(e => e.id == nodes[key].id))){
+            delNodes.push(nodes[key]);
+        }
+    }
+    for(var key in delNodes){
+        deleteNode(delNodes[key]);
+    }
+    //Check if updated nodes and nodes are the same
+    for(var key in uNodes){
+        if(!(nodes.some(e => e.id == uNodes[key].id))){
+            addNode(parseInt(uNodes[key].id));
+        }
+    }
+    // Update Edges in seperate function to guarantee execution after nodes were created
+    updateEdges(uLinks);
+    if(edgeUpdate){
+        loadXml();
+    }
+}
 
+// Update Edges in visualization
+function updateEdges(uLinks){
+    let delLinks = [];
+    const { nodes, links } = forceGraph.graphData();
+    for(var key in links){
+        if(!(uLinks.some(e => (e.source == links[key].source.id) && (e.target == links[key].target.id)))){
+            delLinks.push(links[key]);
+        }
+    }
+    for(var key in delLinks){
+        var index = links.indexOf(delLinks[key]);
+        links.splice(index, 1);
+    }
+    for(var key in uLinks){
+        if(!(links.some(e => (e.source.id == uLinks[key].source) && (e.target.id == uLinks[key].target)))){
+            var source = uLinks[key].source;
+            var target = uLinks[key].target;
+            var src = nodes.find(e => e.id == source);
+            var trg = nodes.find(e => e.id == target);
 
-    // }
-    // console.log(nodes);
-    // console.log(uNodes);
-    // if(uNodes.length != 0){
-
-    //     forceGraph.graphData({
-    //         nodes: [...nodes,...uNodes],
-    //         links: links
-    //     });
-    // }
-
-    // if(edgeUpdate){
-    //     loadXml(false);
-    // }
-    
-    
-
-    // if(!host){
-    //     edgeFormat = JSON.parse(JSON.stringify(links[0]));
-    // }
+            addEdge(src, trg);
+        }
+    }
 }
 
 // Translation functions to synchronize text and vis. graph editor
 // Translate the json graph to a XML-Based version
-
-function jsonToXmlL(json, force){
+function jsonToXmlL(json){
     var xmlString = '';
     let nodes = json.nodes;
     let links = json.links;
@@ -226,12 +257,8 @@ function jsonToXmlL(json, force){
         xmlString += '<node id="' + nodes[key].id + '"/>\n';
     }
     for(var key in links){
-        if(force){
-            xmlString += '\n<edge source="' + links[key].source.id + '" target="' + links[key].target.id + '"/>';
-        } else {
-            xmlString += '\n<edge source="' + links[key].source + '" target="' + links[key].target + '"/>';
-        }
-        
+        xmlString += '\n<edge source="' + links[key].source.id + '" target="' + links[key].target.id + '"/>';
+
     }
     return xmlString
 }
@@ -259,28 +286,36 @@ function jsonToXml(){
  */
 function xmlToJson(xml){
     let translation = JSON.parse(convert.xml2json(xml, {compact: true, spaces:2}));
-    let nodes = translation.node;
-    let links = translation.edge;
+    let tNodes = translation.node;
+    let tLinks = translation.edge;
     let nodeIds = [];
     let visualization = {
         nodes: [],
         links: []
     };
-    for(var key in nodes){
-        let curId = nodes[key]._attributes.id;
-        nodeIds.push(curId);
-        visualization.nodes.push( {color: 'rgba(204.0,204.0,255.0,1.0)', size: 20, name: key, id: curId, label: ''} );
-    }
-    for(var key in links){
-        let src = links[key]._attributes.source;
-        let trg = links[key]._attributes.target;
-
-        // Filter out edges of nodes that no longer exist
-        if((nodeIds.some(e => e == src)) && (nodeIds.some(e => e == trg))){
-            visualization.links.push( {style: 0, source: src, target: trg} );
+    translate:{
+        for(var key in tNodes){
+            let curId = tNodes[key]._attributes.id;
+            if(nodeIds.some(e => e == curId)){
+                alert("Error: Id's must be unique: Id " + curId + " used multiple times!");
+                break translate;
+            } else {
+                nodeIds.push(curId);
+            }
+            
+            visualization.nodes.push( {color: 'rgba(204.0,204.0,255.0,1.0)', size: 20, name: key, id: curId, label: ''} );
         }
+        for(var key in tLinks){
+            let src = tLinks[key]._attributes.source;
+            let trg = tLinks[key]._attributes.target;
+
+            // Filter out edges of nodes that no longer exist
+            if((nodeIds.some(e => e == src)) && (nodeIds.some(e => e == trg))){
+                visualization.links.push( {style: 0, source: src, target: trg} );
+            }
+        }
+        return visualization;
     }
-    return visualization;
 }
 
 function downloadString(text, fileType, fileName) {
@@ -303,12 +338,6 @@ function downloadString(text, fileType, fileName) {
  * Initializes the Visualization with all the functionalities and the design
  */
 function getForceVis() {
-    var links = initialGraph.links;
-
-    if(host) {
-        edgeFormat = JSON.parse(JSON.stringify(links[0]));
-    }
-
     const elem = document.getElementById("editForceVisualizationGraphic");
 
     // First initialization of the Visualiuation
@@ -342,121 +371,105 @@ function getForceVis() {
     }
 }
 // Functions that are executed by events, e.g. left click on a button
-function addNode() {
+function addNode(ident) {
     const { nodes, links } = forceGraph.graphData();
-    const id = nodes.length;
-    console.log(nodes[1].x);
+    var id;
+    if(typeof ident == "number"){
+        id = ident;
+    } else {
+        id = Math.max.apply(Math, nodes.map(function(o) { return o.id; }))+1;
+    }
     forceGraph.graphData({
         nodes: [...nodes, {"color":"rgba(204.0,204.0,255.0,1.0)","name":id,"id":id,"label":""}],
         links: links
     });
-    loadXml(true);
+    if(!(typeof ident =="number")){
+        loadXml();
+    }
+    
 }
 
-// remove edge
+// Remove selected edge
 function removeLink(link){
     if(!nodeMode){
         let { nodes, links } = forceGraph.graphData();
-        for(var key in links){
-            if(link.source.id == links[key].source.id && link.target.id == links[key].target.id){
-                links.splice(key,1);
-                if(undirectedChecked){
-                    for(var key2 in links){
-                        if(link.source.id == links[key2].target.id && link.target.id == links[key2].source.id){
-                            links.splice(key2,1);
-                            break;
-                        }
-                    }
-                }
-            }
-
+        // console.log(links);
+        console.log(link);
+        var index = links.indexOf(link);
+        
+        // console.log(index);
+        if(link == links[index] && index != -1){
+            links.splice(index,1);
         }
-        forceGraph.graphData({ nodes, links });
-        loadXml(true); 
+        
+        if(undirectedChecked){
+            var revLink = links.find(e => e.source == link.target && e.target == link.source);
+            var revIndex = links.indexOf(revLink);
+            if(revLink == links[revIndex] && revIndex != -1){
+                links.splice(revIndex,1);
+            }
+        }
+        loadXml();
     }             
 }
-
+function deleteNode(node){
+    let {nodes, links} = forceGraph.graphData();
+    links = links.filter(l => l.source !== node && l.target !== node);
+    for(var key in nodes){
+        if(node.id == nodes[key].id){
+            nodes.splice(key, 1);
+        }
+    }
+    // nodes.splice(node.id,1);
+    // nodes.forEach((n,idx) => {n.id = idx; });
+    forceGraph.graphData({ nodes, links });  
+    for(var n in nodes){
+        nodes[n].name = nodes[n].id.toString();
+    }
+}
+function addEdge(source, target){
+    const{ nodes, links } = forceGraph.graphData();
+    var edge = {};
+    edge.target = target;
+    edge.source = source;
+    if(links.some(e => (e.source.id == edge.source.id) && (e.target.id == edge.target.id))){
+        alert("Edge already exists!");
+    } else {
+        forceGraph.graphData({
+            nodes: nodes,
+            links: [...links, edge]
+        })
+    }
+}
 function editNode(node) {
     if(nodeMode){
-        let {nodes, links} = forceGraph.graphData();
-        links = links.filter(l => l.source !== node && l.target !== node);
-        nodes.splice(node.id,1);
-        nodes.forEach((n,idx) => {n.id = idx; });
-        forceGraph.graphData({ nodes, links });  
-        for(var n in nodes){
-            nodes[n].name = nodes[n].id.toString();
-        }
-        loadXml(true); 
+        deleteNode(node);
     // Select node or connect 2 selected nodes
     // Connect 2 nodes in both directions if undirected box is checked
-    }else {
+    } else {
         // If no node is selected
         if(!nodeSelected){
             lastNode = node;
             node.color = "orange";
+            
         // if the selected nod e is clicked again, unselect it
         } else if(lastNode == node){
-            lastNode.color = "rgba(204.0,204.0,255.0,1.0)";  
+            lastNode.color = "rgba(204.0,204.0,255.0,1.0)"; 
+
         // Connect two selected nodes with directed edge
         } else if(!undirectedChecked) {
-            const{ nodes, links } = forceGraph.graphData();
-            var containsEdge = false;
-            var edge = JSON.parse(JSON.stringify(edgeFormat));
-            edge.target = node;
-            edge.source = lastNode;
-            for(key in links){
-                if(links[key].source.id == edge.source.id && links[key].target.id == edge.target.id){
-                    containsEdge = true;
-                    break;
-                }
-            }
-            if(!containsEdge){
-                forceGraph.graphData({
-                    nodes: nodes,
-                    links: [...links, edge]
-                })
-                lastNode.color = "rgba(204.0,204.0,255.0,1.0)";
-            } else {
-                lastNode.color = "rgba(204.0,204.0,255.0,1.0)";
-                alert("Edge already exists!");
-            }
-            loadXml(true);     
+            addEdge(lastNode, node);
+            lastNode.color = "rgba(204.0,204.0,255.0,1.0)";
+
         // Connect two selected nodes with undirected edge
         } else if(undirectedChecked){
-            const{ nodes, links } = forceGraph.graphData();
-            var containsEdge = false;
-            var secondEdge = JSON.parse(JSON.stringify(edgeFormat));
-            var edge = JSON.parse(JSON.stringify(edgeFormat));
-            edge.target = node;
-            edge.source = lastNode;
-            secondEdge.source = node;
-            secondEdge.target = lastNode;
-            for(var key in links){
-                if((links[key].source.id == edge.source.id && links[key].target.id == edge.target.id)){
-                    containsEdge = true;
-                    break;
-                }
-            }
-            for(var key in links){
-                if((links[key].source.id == secondEdge.source.id && links[key].target.id == secondEdge.target.id)){
-                    containsEdge = true;
-                    break;
-                }
-            }
-            if(!containsEdge){
-                    forceGraph.graphData({
-                    nodes: nodes,
-                    links: [...links, edge, secondEdge]
-                    })
-                    lastNode.color = "rgba(204.0,204.0,255.0,1.0)"; 
-            } else {
-                lastNode.color = "rgba(204.0,204.0,255.0,1.0)"; 
-                alert("Edge already exists!");
-            }   
-            loadXml(true);  
-        }                        
+            addEdge(node,lastNode);
+            addEdge(lastNode, node);
+            lastNode.color = "rgba(204.0,204.0,255.0,1.0)";
+        }                    
         nodeSelected = !nodeSelected;
-    }                    
+    } 
+    loadXml();                    
 }
 
 
@@ -516,3 +529,34 @@ document.addEventListener('keydown', e => {
         editor.setCursor({line: cursorPos.line, ch: cursorPos.ch});
     }
   });
+
+tutorial = `Welcome to the collaborative graph editor:
+Visual Editor:
+-Use right click to add nodes and left click to delete them.
+
+-By switching to edge mode (Node Mode button), you can delete edges 
+by clicking on them and add edges by selecting 2 nodes
+
+-If you want to undo your changes, you can click on Reload Graph
+to get back to the initial graph. Please note that this will reload the graph, 
+so it will load the last graph you saved!
+
+-Clicking on the undirected checkbox will enable undirected mode. 
+Here the edges are displayed without arrows and adding/deleting edges will affect both directions.
+
+Text Editor:
+-Here you can edit the xml-based code of your graph.
+
+-You can add edges by adding a "<node id="id"/>" or "<node id="id"></node>" tag.
+Edges need a target and source attribute.
+
+-Auto translate is enabled by default, however, you can disable it to save some RAM.
+
+Collaborative session:
+-Copy the invite link and send it to your colleagues. 
+They will need a learning layers account to get access.
+
+(The collaborative session is hostet on your browsers, 
+make sure you save your progress before closing your browser)
+
+Click on insert graph to begin`;
